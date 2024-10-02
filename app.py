086ba1f57ddd519a1881e9c98a05f84203e1ad66
charan15076 <charanjalukuru15076@gmail.com>
2024-10-02 15:55:14 -0700

    Integrated Audio Handler

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 from llm_chains import load_normal_chain
 from langchain.memory import StreamlitChatMessageHistory
+from streamlit_mic_recorder import mic_recorder
+from utils import save_chat_history_json, get_timestamp, load_chat_history_json
+from audio_handler import transcribe_audio
+import yaml
+import os
+
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
 
 
 def load_chain(chat_history):
@@ -14,29 +22,82 @@ def set_send_input():
     st.session_state.send_input = True
     clear_input_field()
 
+def track_index():
+    st.session_state.session_index_tracker = st.session_state.session_key
+
+def save_chat_history():
+
+    if st.session_state.history != []:
+        if st.session_state.session_key == "new_session":
+            st.session_state.new_session_key = get_timestamp()+".json"
+            save_chat_history_json(st.session_state.history, config["chat_history_path"]+st.session_state.new_session_key)
+        else:
+            save_chat_history_json(st.session_state.history, config["chat_history_path"]+st.session_state.session_key)
+
+
 def main():
     st.title("Mutlimodal Local Chat App")
     chat_container = st.container()
+    st.sidebar.title("Chat Sessions")
+    chat_sessions = ["new_session"]+os.listdir(config["chat_history_path"])
+
 
     if "send_input" not in st.session_state:
+        st.session_state.session_key = "new_session"
         st.session_state.send_input = False
         st.session_state.user_question = ""
+        st.session_state.new_session_key = None
+        st.session_state.session_index_tracker = "new_session"
+    if st.session_state.session_key == "new_session" and st.session_state.new_session_key != None:
+        st.session_state.session_index_tracker = st.session_state.new_session_key
+        st.session_state.new_session_key = None
+    
+    index = chat_sessions.index(st.session_state.session_index_tracker)
+    st.sidebar.selectbox("Select a chat session",chat_sessions, key="session_key", index = index, on_change=track_index)
+
+    if st.session_state.session_key != "new_session":
+        file_path = config["chat_history_path"]+st.session_state.session_key
+        st.session_state.history = load_chat_history_json(file_path)
+    else:
+        st.session_state.history = []
 
     chat_history = StreamlitChatMessageHistory(key = "history")
     llm_chain = load_chain(chat_history)
     user_input = st.text_input("Type your input here", key = "user_input", on_change = set_send_input)
+    voice_recording_column, send_button_column = st.columns(2)
+    with voice_recording_column:
+        voice_recording = mic_recorder(
+            start_prompt="Start recording",
+            stop_prompt="Stop recording",
+            just_once=True
+        )
+    with send_button_column:
+        send_button = st.button("Send", key = "send_button", on_click=clear_input_field)
 
-    send_button = st.button("Send", key = 'send_button')
+    uploaded_audio = st.sidebar.file_uploader("Upload an audio file",type=["wav","mp3","ogg"])
+
+    if uploaded_audio:
+        transcribed_audio = transcribe_audio(uploaded_audio.getvalue())
+        print(transcribed_audio)
+        llm_chain.run("Summarize this text: "+transcribed_audio)
+
+    
+    if voice_recording:
+        transcribed_audio = transcribe_audio(voice_recording["bytes"])
+        llm_chain.run(transcribed_audio)
 
     if send_button or st.session_state.send_input:
         if st.session_state.user_question != "":
-            llm_response = "This is a response from LLM model"
+            llm_response = llm_chain.run(st.session_state.user_question)
+            st.session_state.user_question = ""
+    
+    if chat_history.messages != []:
+        with chat_container:
+            st.write("Chat History:")
+            for message in chat_history.messages:
+                st.chat_message(message.type).write(message.content)
 
-            with chat_container:
-                st.chat_message("user").write(st.session_state.user_question)
-                llm_response = llm_chain.run(st.session_state.user_question)
-                st.chat_message("ai").write(llm_response)
-                st.session_state.user_question = ""
+    save_chat_history()
 
 
 if __name__ == "__main__":
