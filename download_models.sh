26ca8670ecd80032427ccda10b2bae8bc26fe78e
charan15076 <charanjalukuru15076@gmail.com>
2024-12-09 17:26:32 -0800

    ecr push

--- download_models.sh
+++ download_models.sh
@@ -6,7 +6,7 @@ mkdir -p models
 wget -O models/mistral-7b-instruct-v0.1.Q3_K_M.gguf https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q3_K_M.gguf
 
 # Download LLaVA 13B model
-wget -O models/ggml-model-q5_k.gguf https://huggingface.co/mys/ggml_llava-v1.5-13b/resolve/main/ggml-model-q5_k.gguf
+#wget -O models/ggml-model-q5_k.gguf https://huggingface.co/mys/ggml_llava-v1.5-13b/resolve/main/ggml-model-q5_k.gguf
 
 # Download LLaVA 7B model
-wget -O models/mmproj-model-f16.gguf https://huggingface.co/mys/ggml_llava-v1.5-7b/resolve/main/mmproj-model-f16.gguf
+#wget -O models/mmproj-model-f16.gguf https://huggingface.co/mys/ggml_llava-v1.5-7b/resolve/main/mmproj-model-f16.gguf
