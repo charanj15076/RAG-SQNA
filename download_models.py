5928943193e2aef1d683fc20d774898b3b73abfc
charan15076 <charanjalukuru15076@gmail.com>
2024-12-12 22:10:02 -0800

    demo

--- download_models.py
+++ download_models.py
@@ -8,7 +8,7 @@ def download_model(model_url, save_path):
     print(f"Downloaded model to {save_path}")
 
 if __name__ == "__main__":
-    # URLs of your models
+
     models = {
         #https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q5_K_M.gguf?download=true
         #https://huggingface.co/mys/ggml_llava-v1.5-13b/resolve/main/ggml-model-q5_k.gguf?download=true
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         "ggml-model-q5_k": "https://huggingface.co/mys/ggml_llava-v1.5-13b/resolve/main/ggml-model-q5_k.gguf?download=true",
         "mmproj-model-f16" : "https://huggingface.co/mys/ggml_llava-v1.5-7b/resolve/main/mmproj-model-f16.gguf?download=true"
 
-        # Add other model URLs here
+     
     }
 
     for model_name, model_url in models.items():
--- testing.py
+++ testing.py
@@ -1,12 +1,26 @@
-import boto3
-from langchain_community.embeddings import BedrockEmbeddings
+from transformers import BlipProcessor, BlipForConditionalGeneration, pipeline
+import base64
+from utils import load_config
 
+config = load_config()
 
+def convert_bytes_to_base64(image_bytes):
+    encoded_string = base64.b64encode(image_bytes).decode("utf-8")
+    return "data:image/jpeg;base64," + encoded_string
 
-bedrock_client = boto3.client(service_name="bedrock-runtime")
-bedrock_embedding = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client=bedrock_client)
+def handle_image(image_bytes, user_message):
+    # Load BLIP model and processor from Hugging Face
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
-# mistral.mistral-7b-instruct-v0:2
+    # Convert image to base64 for integration
+    image_base64 = convert_bytes_to_base64(image_bytes)
 
+    # Process the image and text to generate a caption or response
+    inputs = processor(image_bytes, user_message, return_tensors="pt")
 
+    # Generate output using the Hugging Face model
+    out = model.generate(**inputs)
+    caption = processor.decode(out[0], skip_special_tokens=True)
 
+    return caption
